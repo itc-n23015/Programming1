af7388d317f5751044e2a020c5f60a9f93241002
itc-n23015 <n23015@std.it-college.ac.jp>
2023-07-07 01:08:59 +0000

    2.6

---
+++ CHAPTER02/2.6method/lesson/UsedIndex.py
@@ -0,0 +1,10 @@
+my_str = 'abcあいうえ'
+print(my_str[0])
+print(my_str[4])
+print(my_str[-2])
+print(my_str[-5])
+
+
+print(len(my_str))
+
+#文字列はイミュータブル(immutable)
---
+++ CHAPTER02/2.6method/lesson/format.py
@@ -0,0 +1,20 @@
+a = 'abc'
+b = 'xyz'
+print("{} {}".format(a, b))
+
+
+title = 'name'
+value = 'John Smith'
+print('Your {} is {}.'.format(title, value))
+
+age = 20
+print('your age is {}.'.format(age))
+
+
+title = 'IQ'
+value = 100
+print('{} = {}'.format(title, value))
+
+#フォーマット済みリテラル
+a, b = "Hello", "World"
+print(f"{a} {b}")
