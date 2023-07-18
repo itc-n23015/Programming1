013791f7dda2d4cc93aef86c9fd3e33e7b74d6dd
itc-n23015 <n23015@std.it-college.ac.jp>
2023-07-18 02:56:48 +0000

    修正

--- CHAPTER02/Q2_final.py
+++ CHAPTER02/Q2_final.py
@@ -1,5 +1,6 @@
-name = 's'
-tr = [chr(i) for i in range(ord('a'),ord('z')+1)]
-while not(tr == name):
-    break
+import random
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+name = random.choice(alphabet)
+while not(name == 's'):
+    name = random.choice(alphabet)
 print(name)
